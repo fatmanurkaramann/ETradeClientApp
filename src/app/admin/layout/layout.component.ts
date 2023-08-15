2d0e2b7552785ffa17ea5978e795ad82398a104f
Fatma Nur Karaman <fatmanur.karaman@outlook.com>
2023-08-15 15:19:23 +0300

    Angular standAlone yapısı kuruldu

--- src/app/admin/layout/layout.component.ts
+++ src/app/admin/layout/layout.component.ts
@@ -1,9 +1,15 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from './components/header/header.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-layout',
   templateUrl: './layout.component.html',
-  styleUrls: ['./layout.component.css']
+  styleUrls: ['./layout.component.css'],
+  standalone:true,
+  imports:[HeaderComponent,SidebarComponent,FooterComponent,RouterModule]
 })
 export class LayoutComponent {
 
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,24 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-import { AppModule } from './app/app.module';
+import { bootstrapApplication } from '@angular/platform-browser';
+import { AppComponent } from './app/app.component';
+import { importProvidersFrom } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+
+bootstrapApplication(AppComponent,{
+  providers:
+  [
+    importProvidersFrom(CommonModule,
+      RouterModule.forRoot([
+        {path:'',loadComponent:()=>import("./app/admin/layout/layout.component").then(c=>c.LayoutComponent)},
+        {path:'products',loadComponent:()=>import("./app/admin/components/products/products.component").then(c=>c.ProductsComponent)},
+        {path:'customer',loadComponent:()=>import("./app/admin/components/customer/customer.component").then(c=>c.CustomerComponent)},
+
+      ])
+
+      )
+  ]
+})
