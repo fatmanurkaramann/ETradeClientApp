3e02d01a192e3e2cba2635904b6f9c1612f8472f
Fatma Nur Karaman <fatmanur.karaman@outlook.com>
2023-09-03 16:26:54 +0300

    ngx file kütüphanesi indirildi

--- src/app/admin/components/products/create/create.component.ts
+++ src/app/admin/components/products/create/create.component.ts
@@ -16,10 +16,11 @@ import { MatButtonModule } from '@angular/material/button';
 import { ProductService } from 'src/app/services/common/models/product.service';
 import { CreateProduct } from 'src/app/contracts/create_product';
 import { CustomToastrService, MessageType } from 'src/app/services/ui/custom-toastr.service';
+import { FileUploadComponent } from 'src/app/services/common/file-upload/file-upload.component';
 @Component({
   selector: 'app-create',
   standalone: true,
-  imports: [CommonModule,FormsModule, MatFormFieldModule, MatInputModule, ReactiveFormsModule, NgIf,MatButtonModule],
+  imports: [CommonModule,FormsModule, MatFormFieldModule, MatInputModule, ReactiveFormsModule, NgIf,MatButtonModule,FileUploadComponent],
   templateUrl: './create.component.html',
   styleUrls: ['./create.component.css']
 })
