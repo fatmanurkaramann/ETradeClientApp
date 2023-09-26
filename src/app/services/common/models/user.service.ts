3ae3da10d7e864351e3f116a685a96655389d8ce
Fatma Nur Karaman <fatmanur.karaman@outlook.com>
2023-09-26 23:28:16 +0300

    servise login işlemi eklendi.

--- src/app/services/common/models/user.service.ts
+++ src/app/services/common/models/user.service.ts
@@ -3,6 +3,7 @@ import { HttpClientService } from '../http-client.service';
 import { User } from 'src/app/entities/user';
 import { Create_User } from 'src/app/contracts/users/create_user';
 import { Observable, firstValueFrom } from 'rxjs';
+import { Token } from 'src/app/contracts/token/token';
 
 @Injectable({
   providedIn: 'root',
@@ -12,14 +13,24 @@ export class UserService {
 
   async create(user: User): Promise<Create_User> {
     try {
-      const obs: Observable<Create_User | User> = this.httpClientService.post<Create_User | User>({ controller: 'users' }, user);
-      const response = await firstValueFrom(obs) as Create_User;
+      const obs: Observable<Create_User | User> = this.httpClientService.post<
+        Create_User | User
+      >({ controller: 'users' }, user);
+      const response = (await firstValueFrom(obs)) as Create_User;
       console.log('API Response:', response);
       return response;
     } catch (error) {
       console.error('API Error:', error);
-      throw error; // Rethrow the error to handle it in the component.
+      throw error;
     }
+  }
+  async login(userNameOrEmail:string, password:string,callbackFunction?:()=>void) :Promise<any | Token> {
+   const obs = this.httpClientService.post<any | Token>(
+      { controller: 'users', action: 'login' },
+      { userNameOrEmail, password }
+    );
+   await firstValueFrom(obs)
+   callbackFunction();
   }
 
 }
--- src/app/ui/components/login/login.component.ts
+++ src/app/ui/components/login/login.component.ts
@@ -1,13 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { UserService } from 'src/app/services/common/models/user.service';
+import { GlobalToastrConfig } from 'ngx-toastr';
+import { CustomToastrService, MessageType } from 'src/app/services/ui/custom-toastr.service';
 
 @Component({
   selector: 'app-login',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
-export class LoginComponent {
-
+export class LoginComponent implements OnInit {
+  constructor(private userService: UserService,private toastr:CustomToastrService) {}
+  ngOnInit(): void {
+  }
+  isAuth:boolean=false
+  async login(userNameOrEmail: string, password: string) {
+    await this.userService.login(userNameOrEmail, password,()=>{
+      this.isAuth=true;
+      this.toastr.message("Giriş başarılı","",MessageType.Success)
+    });
+  }
 }
